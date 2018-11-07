0948b071b13f7654ffd396e1bcc13f59935f2d95
Vitalik Makh <patriot02faqq@gmail.com>
2018-11-07 13:38:48 +0200

    task 29

---
+++ src/main/java/com/olhastfn/tasks/task1223/Solution.java
@@ -0,0 +1,7 @@
+package com.olhastfn.tasks.task1223;
+
+/**
+ * Created on 07.11.2018.
+ */
+public class Solution {
+}
---
+++ src/main/java/com/olhastfn/tasks/task1225/Solution.java
@@ -0,0 +1,7 @@
+package com.olhastfn.tasks.task1225;
+
+/**
+ * Created on 07.11.2018.
+ */
+public class Solution {
+}
