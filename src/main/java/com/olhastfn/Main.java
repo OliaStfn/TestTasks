43677a07b11fbd0c4f3772bbbc53a52437cf8a00
OliaStfn <kardash2203@gmail.com>
2018-12-23 21:55:05 +0200

    union

--- src/main/java/com/olhastfn/Main.java
+++ src/main/java/com/olhastfn/Main.java
@@ -1,7 +1,17 @@
 package com.olhastfn;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         System.out.println("It`s a magic");
+        Object f = new Object();
+
+        for(int i=0; i < 200; i++){
+            System.out.println("fgdfgdf");
+            if (i==167) throw new Exception();
+        }
+
+
+
+        System.out.println("gdfgdfgf");
     }
 }
